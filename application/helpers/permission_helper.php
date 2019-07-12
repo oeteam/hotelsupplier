38c2b64de2ab7f627efa77297df22c1b97e242d0
subinrabin <subinrabin@gmail.com>
2019-07-12 12:10:37 +0530

    permission set

--- application/helpers/permission_helper.php
+++ application/helpers/permission_helper.php
@@ -2,8 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
  
 $ci =& get_instance();
-$validate = $ci->db->query("Select id from hotel_tbl_agents where id= '".$ci->session->userdata('agent_id')."' and extranetStatus = 1 and delflg = 1");
-$validate = $validate->result();
-if(count($validate) ==0) {
-  redirect(base_url()."/welcome/agent_logout");
+if ($ci->session->userdata('supplier_id')!="") {
+	$validate = $ci->db->query("Select id from hotel_tbl_agents where id= '".$ci->session->userdata('supplier_id')."' and extranetStatus = 1 and delflg = 1");
+	$validate = $validate->result();
+	if(count($validate) ==0) {
+	  redirect(base_url()."/welcome/agent_logout");
+	}
 }
